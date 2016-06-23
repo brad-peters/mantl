1748275029ca21807d32c424d3dc40d9ff02df20
Brad Peters <brad.peters@ca.ibm.com>
2016-06-23 16:27:00 -0300

    Changes marathon to use vault function for token renew service

--- roles/marathon/files/vault-wrapper-marathon.sh
+++ roles/marathon/files/vault-wrapper-marathon.sh
@@ -3,9 +3,15 @@
 # set vault values to disk
 vault read -field=mesos_framework_auth secret/marathon/mesos_framework_auth/ > /tmp/marathon-mesos-authentication-secret
 
+# pack docker auth for marathon
+mkdir /tmp/docker_auth
+vault read -field=auth secret/repositories/docker/auth > /tmp/docker_auth/config.json
+tar cvzf /tmp/docker.tar.gz /tmp/docker_auth
+
 # set env vars from vault
 export MARATHON_ZK=$(vault read -field=value secret/marathon/marathon_zk_connect)
 export MARATHON_MASTER=$(vault read -field=value secret/marathon/marathon_mesos)
+
 
 # start service
 /usr/bin/marathon
