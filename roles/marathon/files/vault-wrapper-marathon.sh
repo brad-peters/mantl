8ed683768ebbc0568fdba04f991a4cf4ae241ba9
Brad Peters <brad.peters@ca.ibm.com>
2016-06-27 16:23:58 -0300

    Changes marathon to work with remote vault address

--- roles/marathon/files/vault-wrapper-marathon.sh
+++ roles/marathon/files/vault-wrapper-marathon.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # set vault values to disk
-vault read -field=mesos_framework_auth secret/marathon/mesos_framework_auth/ > /tmp/marathon-mesos-authentication-secret
-
-# pack docker auth for marathon
-mkdir /tmp/docker_auth
-vault read -field=auth secret/repositories/docker/auth > /tmp/docker_auth/config.json
-tar cvzf /tmp/docker.tar.gz /tmp/docker_auth
+vault read -field=value secret/marathon/mesos_framework_auth > /tmp/marathon-mesos-authentication-secret
 
 # set env vars from vault
 export MARATHON_ZK=$(vault read -field=value secret/marathon/marathon_zk_connect)
 export MARATHON_MASTER=$(vault read -field=value secret/marathon/marathon_mesos)
 
+# pack docker auth for marathon
+mkdir -p /tmp/docker_auth/.docker
+vault read -field=value secret/repositories/docker/auth > /tmp/docker_auth/.docker/config.json
+cd /tmp/docker_auth
+tar cvzf /tmp/docker.tar.gz .docker
 
 # start service
 /usr/bin/marathon
